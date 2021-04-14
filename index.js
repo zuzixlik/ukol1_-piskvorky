43475f65a82f9eb0dfb663f25675e3e2bce8f5a0
Zuzana Babkova <babkova.zuzana007@gmail.com>
2021-04-14 14:36:27 +0200

    test s konzoly

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 'Use strict';
 
 console.log('Funguji');
+
+let playing = 'circle';
+
+let click = 0;
+const playsElm = document.querySelector('.hra--circle');
+console.log(playsElm);
+
+/*
+const selectButton = (e) => {
+  console.log('kliknuto na policko');
+};*/
