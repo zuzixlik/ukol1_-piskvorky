a3f55f5b8e7f89d038195e27caaec0a1cb59aabc
Zuzana Babkova <babkova.zuzana007@gmail.com>
2021-04-15 20:12:02 +0200

    krizky-kolecka-cyklus

--- index.js
+++ index.js
@@ -2,13 +2,26 @@
 
 console.log('Funguji');
 
-let playing = 'circle';
+let tedHraje = 'circle';
 
-let click = 0;
-const playsElm = document.querySelector('.hra--circle');
-console.log(playsElm);
+const HrajeElm = document.querySelector('.hra--circle');
 
-/*
-const selectButton = (e) => {
-  console.log('kliknuto na policko');
-};*/
+const rozehrano = (e) => {
+  if (tedHraje === 'circle') {
+    e.target.classList.add('policko_hraje_kolecko');
+    e.target.disabled = true;
+    HrajeElm.src = 'obrazky/cross.svg';
+    tedHraje = 'cross';
+  } else if (tedHraje === 'cross') {
+    e.target.classList.add('policko_hraje_krizek');
+    e.target.disabled = true;
+    HrajeElm.src = 'obrazky/circle.svg';
+    tedHraje = 'circle';
+  }
+};
+/*nevim zda je spravne pouziti br, jelikoz podminka funguje pouze na cely hlavni hraci obsah a ne na jednotlive tlacitka- asi neni dobre puvodni reseni za pomoci zalamovani radku s br */
+
+const pole = document.querySelectorAll('.hra--hlavni--obsah');
+for (let i = 0; i < pole.length; i++) {
+  pole[i].addEventListener('click', rozehrano);
+}
