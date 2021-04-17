3b866cd893f9ee3b5d72f2d20e7f2d66c4a41cbd
Zuzana Babkova <babkova.zuzana007@gmail.com>
2021-04-18 01:18:23 +0200

    upravene html+zacatek JS 5

--- index.js
+++ index.js
@@ -19,9 +19,61 @@ const rozehrano = (e) => {
     tedHraje = 'circle';
   }
 };
-/*nevim zda je spravne pouziti br, jelikoz podminka funguje pouze na cely hlavni hraci obsah a ne na jednotlive tlacitka- asi neni dobre puvodni reseni za pomoci zalamovani radku s br */
 
 const pole = document.querySelectorAll('.hra--hlavni--obsah');
 for (let i = 0; i < pole.length; i++) {
   pole[i].addEventListener('click', rozehrano);
 }
+
+/*ukol c5*/
+const hraHlavniObsahSize = 10; // 10x10
+
+const getPosition = (policko) => {
+  let polickoIndex = 0;
+  while (polickoIndex < HrajeElm.length) {
+    if (policko === HrajeElm[polickoIndex]) {
+      break;
+    }
+    polickoIndex++;
+  }
+  return {
+    row: Math.floor(polickoIndex / hraHlavniObsahSize),
+    column: polickoIndex % hraHlavniObsahSize,
+  };
+};
+
+/*Pro detekci výherního tahu budeš potřebovat několik pomocných funkcí.
+
+const field = [{ row: 0, column: 0 }];
+
+console.log(getPosition(nejakyButtonElement));
+
+const hraHlavniObsahSize = 10; // 10x10
+const hraHlavniObsah = document.querySelectorAll('.policko'); // Selektor pozměň tak, aby odpovídal tvému kódu.
+const getPosition = (policko) => {
+  let polickoIndex = 0;
+  while (polickoIndex < hraHlavniObsah.length) {
+    if (policko === hraHlavniObsah[polickoIndex]) {
+      break;
+    }
+    polickoIndex++;
+  }
+  return {
+    row: Math.floor(polickoIndex / hraHlavniObsahSize),
+    column: polickoIndex % hraHlavniObsahSize,
+  };
+};
+
+console.log(getPosition('policko'));
+
+const handleClick = () => {
+  console.log(getPosition('policko'));
+};
+document.addEventListener('click', handleClick);
+
+/*
+const getField = (row, column) => {
+  hraHlavniObsah[row * hraHlavniObsahSize + column];
+};
+
+console.log(getField);*/
